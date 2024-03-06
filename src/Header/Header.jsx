2a2bc74d60ae21535dfc8aab7063717afc6cbd55
BlackDamask <148754605+BlackDamask@users.noreply.github.com>
2024-03-06 17:39:57 +0100

    In search for design...

---
+++ src/Header/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css'
+
+export default function Header(){
+    return(
+        <div id='header'>
+            <h1>
+                Days Of Weeks
+            </h1>
+        </div>
+    );
+}
