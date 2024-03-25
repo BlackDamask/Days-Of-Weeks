974563cca445f6714fca8e9258d8d5d34115701e
BlackDamask <148754605+BlackDamask@users.noreply.github.com>
2024-03-25 19:00:32 +0100

    header

--- src/components/ContentPage/ContentPage.jsx
+++ src/components/ContentPage/ContentPage.jsx
@@ -1,15 +1,41 @@
 import Square from './square/Square.jsx';
 import './ContentPage.css';
-import { getSquares } from '../../data.js';
+import HintArrow from '../../assets/hint_arrow.png'
+import { getSquares, setSquareColor } from '../../data.js';
 
 export default function ContentPage(){
-    const squares = getSquares();
+    let squares = getSquares();
+    console.log(squares);
+    squares = setSquareColor(0,0,'#ffffff');
+    console.log(squares);
+    const ShowHintYear = (rowIndex) =>{
+        const year = rowIndex +1;
+        let answer = <p></p>; 
+        if(year % 5 == 0 && year != 5){
+            answer = <p>{year}</p>;
+        }
+        else if (year == 5){
+            answer = <p>5</p>;
+        }
+        return answer;
+    }
+
     return(
         <div id = 'content-page'>
+            
             <ol id = 'all-squares'>
+                <li id='hint' key = {'hint'}>
+                    <h2>
+                        Weeks of year
+                    </h2>
+                    <img src={HintArrow} alt="hint arrow" />
+                </li>
                 {squares.map((row, rowIndex) =>(
                 <li key = {rowIndex}>
                     <ol className='row-squares'>
+                        <li key={'hintYear'+rowIndex}>
+                            {ShowHintYear(rowIndex)}
+                        </li>
                         {row.map((color, colIndex) => (
                             <li key={colIndex}>
                                 <Square backgroundColor = {color}></Square>
--- src/data.js
+++ src/data.js
@@ -1,9 +1,15 @@
+const squaresWithColors = [];
+let squares = [];
+export function setSquareColor(row,col,color){
+    squares[row][col] = color;
+    return squares;
+}
+
 export function getSquares() {
-    let squares = [];
     for (let i = 0; i < 90; i++) {
         squares[i] = []; // Initialize inner array
         for (let j = 0; j < 52; j++) {
-            squares[i][j] = '#39a7ff';
+            squares[i][j] = '#ffffff';
         }
     }
     return squares;
